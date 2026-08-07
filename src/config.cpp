746284d5c40b25ca8df5ab7b97812c5ceee22f85
terminalX2 <sathviklolla@gmail.com>
2026-08-07 14:06:49 -0700

    file switch 2

---
+++ src/config.cpp
@@ -0,0 +1,69 @@
+#include "lemlib/api.hpp" // IWYU pragma: keep
+#include "pros/adi.hpp"
+
+pros::Controller cont(pros::E_CONTROLLER_MASTER);
+
+//motors
+pros::MotorGroup l({-1, 2, -3}, pros::MotorGears::blue);
+pros::MotorGroup r({4, -5, 6}, pros::MotorGears::blue);
+pros::MotorGroup lift({-14, 15}, pros::MotorGears::red);
+pros::Motor intake(-12, pros::MotorGears::blue);
+
+pros::adi::DigitalOut claw('A');
+
+// drivetrain settings
+lemlib::Drivetrain dt(&l, // left motor group
+                              &r, // right motor group
+                              14.625, // 10 inch track width
+                              lemlib::Omniwheel::NEW_275, // using new 4" omnis
+                              450, // drivetrain rpm is 360
+                              2 // horizontal drift is 2 (for now)
+);
+
+pros::Imu imu(10); // imu
+pros::Rotation horizEnc(20); // horizontal tracking wheel encoder
+pros::Rotation vertEnc(21); // vertical tracking wheel encoder
+
+// horizontal tracking wheel
+lemlib::TrackingWheel horizTrackWheel(&horizEnc, lemlib::Omniwheel::NEW_275, -5.75);
+// vertical tracking wheel
+lemlib::TrackingWheel vertTrackWheel(&vertEnc, lemlib::Omniwheel::NEW_275, -2.5);
+
+// odometry settings
+lemlib::OdomSensors sensors(&vertTrackWheel, // vertical tracking wheel 1, set to null
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+                            &horizTrackWheel, // horizontal tracking wheel 1
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            &imu // inertial sensor
+);
+
+// lateral PID controller
+lemlib::ControllerSettings lateralCont(10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angularCont(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+// create the chassis
+lemlib::Chassis chassis(dt, // drivetrain settings
+                        lateralCont, // lateral PID settings
+                        angularCont, // angular PID settings
+                        sensors // odometry sensors
+);
---
+++ src/main.cpp
@@ -0,0 +1,108 @@
+#include "main.h"
+#include "config.h"
+#include "lemlib/api.hpp" // IWYU pragma: keep
+
+//using namespace pros;
+//using namespace lemlib;
+
+// initialize function. Runs on program startup
+void initialize() {
+    pros::lcd::initialize(); // initialize brain screen
+    chassis.calibrate(); // calibrate sensors
+    // print position to brain screen
+    pros::Task screen_task([&]() {
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            // delay to save resources
+            pros::delay(20);
+        }
+    });
+}
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() {}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() {}
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+
+bool clawState = false;
+
+void controls() {
+    //chassis
+    int forward = cont.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    int turn = cont.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+    chassis.curvature(forward, turn);
+
+    //intake
+    if (cont.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+        intake.move(127);
+    } else if (cont.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+        intake.move(-127);
+    } else {
+        intake.move(0);
+    }
+
+    //lift
+    if (cont.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
+        lift.move(127);
+    } else if (cont.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+        lift.move(-30);
+    } else {
+        lift.move(0);
+        lift.set_brake_mode_all(pros::MotorBrake::brake);
+    }
+
+    //claw
+    if (cont.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)) {
+        clawState = !clawState;
+        claw.set_value(clawState);
+    }
+}
+
+void opcontrol() {
+	while (true) {
+        controls();
+        pros::delay(1);
+	}
+}
